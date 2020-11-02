0c40472a50f839964003c6bc0d57e52bdd60e3b3
Viridiana Jimenez Diaz <vjimenez@eagles.nccu.edu>
2020-11-02 18:59:03 -0500

    solo le cambbie el color

--- primerfileVision.py
+++ primerfileVision.py
@@ -24,7 +24,7 @@ x=[100, 100, 400, 400]
 y=[200, 500, 200, 500]
 
 #plot the points with red star-makers
-plot(x, y, 'g+')
+plot(x, y, 'm+')
 
 #line plot connecting the first two points
 plot(x[:2], y[:2])
