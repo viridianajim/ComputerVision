97dcbb9e03a7733ea145c5a106e7620d739a73a3
Viri Jimenez <vjimenez@eagles.nccu.edu>
2020-11-02 16:25:23 -0500

    the python file

---
+++ primerfileVision.py
@@ -0,0 +1,34 @@
+from PIL import Image
+from pylab import *
+#import os
+#pil_im = Image.open('fruitas.jpg').convert('L')
+
+'''
+#converts all the picture in the file as jps
+for infile in filelist:
+    outfile = os.path.splitext(infile)[0]+".jpg"
+    if infle != otule:
+        try:
+            Image.open(infile).save(outfile)
+        except IOError:
+            print("cannot convert", infile)
+'''
+#read imget to array
+im = array(Image.open('fruitas.jpg'))
+
+#plot the image
+imshow(im)
+
+#some points
+x=[100, 100, 400, 400]
+y=[200, 500, 200, 500]
+
+#plot the points with red star-makers
+plot(x, y, 'g+')
+
+#line plot connecting the first two points
+plot(x[:2], y[:2])
+
+#add title and show the plot
+title('Plotting: "fruitas.jpg"')
+show()
